25865199147761237f99360f698de38302341a25
smedeiros05 <71941814+smedeiros05@users.noreply.github.com>
2022-02-03 00:33:44 -0300

    Initial commit

---
+++ app/src/main/java/com/smedeiros05/conversordemoedas2/ConversaoMoedasActivity.kt
@@ -0,0 +1,11 @@
+package com.smedeiros05.conversordemoedas2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class ConversaoMoedasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
